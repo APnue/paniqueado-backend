33b0f498a740c1649da5098f18871f830306c2bd
APnue <ncer610@gmail.com>
2025-06-02 03:14:47 -0600

    Update get_productos.php

--- api/get_productos.php
+++ api/get_productos.php
@@ -6,25 +6,13 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 // Permitir el encabezado Content-Type para recibir JSON u otros tipos
 header("Access-Control-Allow-Headers: Content-Type");
 
-// Datos de conexión a la base de datos
-$servername = "fdb1028.awardspace.net";
-$username = "4639680_panaderia";
-$password = "y9SW;CKwQ_rhX33";
-$dbname = "4639680_panaderia";
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar si la conexión falló
-if ($conn->connect_error) {
-    die("Error en conexión: " . $conn->connect_error);
-}
+include 'db_connection.php';
 
 // Consulta para obtener todos los productos
 $sql = "SELECT * FROM productos";
 
 // Ejecutar la consulta
-$result = $conn->query($sql);
+$result = $conexion->query($sql);
 
 // Array donde guardaremos todos los productos
 $productos = [];
@@ -38,5 +26,5 @@ while ($row = $result->fetch_assoc()) {
 echo json_encode($productos);
 
 // Cerrar la conexión
-$conn->close();
+$conexion->close();
 ?>
