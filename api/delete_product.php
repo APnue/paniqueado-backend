a7f25849512a051a51ce75896bdcec5e17353389
APnue <ncer610@gmail.com>
2025-06-02 22:56:25 -0600

    Update delete_product.php

--- api/delete_product.php
+++ api/delete_product.php
@@ -10,6 +10,13 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
+// Capturar excepciones y mostrarlas como JSON
+set_exception_handler(function($e) {
+    http_response_code(500);
+    echo json_encode(["success" => false, "error" => $e->getMessage()]);
+    exit();
+});
+
 // Responder a preflight
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
