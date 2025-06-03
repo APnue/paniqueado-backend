39cc9cb90e6901b45e747d91e2e1085e82e5b6af
APnue <ncer610@gmail.com>
2025-06-02 22:12:55 -0600

    Update delete_product.php

--- api/delete_product.php
+++ api/delete_product.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 // Permitir encabezado Content-Type para enviar JSON
 header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
